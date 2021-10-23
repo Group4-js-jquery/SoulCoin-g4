1b9067525a5d404d0b145fca40041335027c6c3f
Eins10 <einstein.hellmeister@gmail.com>
2021-10-23 20:30:05 -0300

    alteracaoDataNasc

---
+++ js/modelo.js
@@ -0,0 +1,107 @@
+<<<<<<< Updated upstream
+=======
+/*******************************************CPF****************************************************/
+function VerificaCPF() {
+    if (vercpf(document.frmcpf.cpf.value)) { document.frmcpf.submit(); } else {
+        errors = "1"; if (errors) alert('CPF NÃO VÁLIDO');
+        document.retorno = (errors == '');
+    }
+}
+function vercpf(cpf) {
+    if (cpf.length != 11 || cpf == "00000000000" || cpf == "11111111111" || cpf == "22222222222" || cpf == "33333333333" || cpf == "44444444444" || cpf == "55555555555" || cpf == "66666666666" || cpf == "77777777777" || cpf == "88888888888" || cpf == "99999999999")
+        return false;
+    add = 0;
+    for (i = 0; i < 9; i++)
+        add += parseInt(cpf.charAt(i)) * (10 - i);
+    rev = 11 - (add % 11);
+    if (rev == 10 || rev == 11)
+        rev = 0;
+    if (rev != parseInt(cpf.charAt(9)))
+        return false;
+    add = 0;
+    for (i = 0; i < 10; i++)
+        add += parseInt(cpf.charAt(i)) * (11 - i);
+    rev = 11 - (add % 11);
+    if (rev == 10 || rev == 11)
+        rev = 0;
+    if (rev != parseInt(cpf.charAt(10)))
+        return false;
+    alert('O CPF INFORMADO É VÁLIDO.'); return true;
+}
+
+/*******************************************CEP****************************************************/
+
+const apresentaDados = (resultado) => {
+    for(let campo in resultado){
+        if(document.querySelector("#" + campo)){
+            console.log(campo);
+            document.querySelector("#" + campo).value = resultado[campo];
+        }
+    }
+}
+
+function consultaCep() {
+    let cepDigitado = document.getElementById("cep");
+
+    if (cepDigitado.value == "") {
+        cepDigitado.style.border = "1px solid red";
+    } else {
+        let cepProcurado = cepDigitado.value.replace("-", "");
+        console.log(cepProcurado);
+
+        fetch(`http://viacep.com.br/ws/${cepProcurado}/json/`)
+            .then(response => {
+                response.json()
+                .then(data => console.log(apresentaDados(data)))
+            })
+            .catch(x => console.log ("CEP não localizado!"))
+    }
+}
+/*******************************************dataNascimento****************************************************/
+$("#btn1").click(function(){
+    let valorDigitado = $("#dataNasc").val();
+
+    // **************************************************************
+    // *********Criando Substrings da string valordigitao************
+    let anoDigitado = "";
+    anoDigitado = valorDigitado.substr(0,4);
+    
+    let mesDigitado = "";
+    mesDigitado = valorDigitado.substr(5,2);
+    
+    let diaDigitado = "";
+    diaDigitado = valorDigitado.substr(8,2);
+
+    let valorAtual=[anoDigitado,mesDigitado,diaDigitado];
+    console.log(valorAtual)
+
+    // **************************************************************
+    // *********Criando função para encontrar a idade ***************
+    function idade(ano_aniversario, mes_aniversario, dia_aniversario) {
+        var d = new Date,
+            ano_atual = d.getFullYear(),
+            mes_atual = d.getMonth() + 1,
+            dia_atual = d.getDate(),
+
+            ano_aniversario = +ano_aniversario,
+            mes_aniversario = +mes_aniversario,
+            dia_aniversario = +dia_aniversario,
+
+            quantos_anos = ano_atual - ano_aniversario;
+
+        if (mes_atual < mes_aniversario || mes_atual == mes_aniversario && dia_atual < dia_aniversario) {
+            quantos_anos--;
+        }
+        return quantos_anos < 0 ? 0 : quantos_anos;
+    }
+    // **************************************************************
+    // *****************Verificador de idade*************************
+    let extrair=(idade(anoDigitado, mesDigitado, diaDigitado))
+    if(extrair<18 || extrair>130){
+        alert("você não está apto a realizar o cadastro")
+    }else{
+        alert("seu cadastro foi aprovado")
+    }
+    console.log(idade(anoDigitado, mesDigitado, diaDigitado)); 
+})            
+>>>>>>> Stashed changes
