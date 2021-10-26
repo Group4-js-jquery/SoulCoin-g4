90ff42bc544fac60520700a149c3d7eb3b93f8d7
aliicebraga <aliicebraga.dev@gmail.com>
2021-10-26 13:52:40 -0300

    13:50_2610
    novo formulário

--- js/modelo.js
+++ js/modelo.js
@@ -39,13 +39,11 @@ function vercpf(cpf) {
 
 /*******************************************CEP****************************************************/
 
-
-const apresentaDados = (resultado) => {
-    for (let campo in resultado) {
-        if (document.querySelector("#" + campo)) {
-            console.log(campo);
-            document.querySelector("#" + campo).value = resultado[campo];
-        }
+const apresentaDados = resultado => {
+  for (let campo in resultado) {
+    if (document.querySelector('#' + campo)) {
+      console.log(campo);
+      document.querySelector('#' + campo).value = resultado[campo];
     }
   }
 };
@@ -160,75 +158,58 @@ function enviardados3() {
   }
 }
 
-    let cepDigitado = document.getElementById("cep");
-
-    if (cepDigitado.value == "") {
-        cepDigitado.style.border = "1px solid red";
-    } else {
-        let cepProcurado = cepDigitado.value.replace("-", "");
-        console.log(cepProcurado);
-
-        fetch(`http://viacep.com.br/ws/${cepProcurado}/json/`)
-            .then(response => {
-                response.json()
-                    .then(data => console.log(apresentaDados(data)))
-            })
-            .catch(x => console.log("CEP não localizado!"))
-    }
-
-}
-
 /*******************************************dataNascimento****************************************************/
 function verificaNasc() {
-    let valorDigitado = $("#dataNasc").val();
+  let valorDigitado = $('#dataNasc').val();
 
-    // **************************************************************
-    // *********Criando Substrings da string valordigitao************
-    let anoDigitado = "";
-    anoDigitado = valorDigitado.substr(0, 4);
+  // **************************************************************
+  // *********Criando Substrings da string valordigitao************
+  let anoDigitado = '';
+  anoDigitado = valorDigitado.substr(0, 4);
 
-    let mesDigitado = "";
-    mesDigitado = valorDigitado.substr(5, 2);
+  let mesDigitado = '';
+  mesDigitado = valorDigitado.substr(5, 2);
 
-    let diaDigitado = "";
-    diaDigitado = valorDigitado.substr(8, 2);
+  let diaDigitado = '';
+  diaDigitado = valorDigitado.substr(8, 2);
 
-    let valorAtual = [anoDigitado, mesDigitado, diaDigitado];
-    console.log(valorAtual)
+  let valorAtual = [anoDigitado, mesDigitado, diaDigitado];
+  console.log(valorAtual);
 
-    // **************************************************************
-    // *********Criando função para encontrar a idade ***************
-    function idade(ano_aniversario, mes_aniversario, dia_aniversario) {
-        var d = new Date,
-            ano_atual = d.getFullYear(),
-            mes_atual = d.getMonth() + 1,
-            dia_atual = d.getDate(),
+  // **************************************************************
+  // *********Criando função para encontrar a idade ***************
+  function idade(ano_aniversario, mes_aniversario, dia_aniversario) {
+    var d = new Date(),
+      ano_atual = d.getFullYear(),
+      mes_atual = d.getMonth() + 1,
+      dia_atual = d.getDate(),
+      ano_aniversario = +ano_aniversario,
+      mes_aniversario = +mes_aniversario,
+      dia_aniversario = +dia_aniversario,
+      quantos_anos = ano_atual - ano_aniversario;
 
-            ano_aniversario = +ano_aniversario,
-            mes_aniversario = +mes_aniversario,
-            dia_aniversario = +dia_aniversario,
-
-            quantos_anos = ano_atual - ano_aniversario;
-
-        if (mes_atual < mes_aniversario || mes_atual == mes_aniversario && dia_atual < dia_aniversario) {
-            quantos_anos--;
-        }
-        return quantos_anos < 0 ? 0 : quantos_anos;
+    if (
+      mes_atual < mes_aniversario ||
+      (mes_atual == mes_aniversario && dia_atual < dia_aniversario)
+    ) {
+      quantos_anos--;
     }
-    // **************************************************************
-    // *****************Verificador de idade*************************
-    let extrair = (idade(anoDigitado, mesDigitado, diaDigitado))
-    if (extrair < 18 || extrair > 130) {
-        alert("você não está apto a realizar o cadastro")
-    } else {
-        alert("seu cadastro foi aprovado")
-    }
-    console.log(idade(anoDigitado, mesDigitado, diaDigitado));
+    return quantos_anos < 0 ? 0 : quantos_anos;
+  }
+  // **************************************************************
+  // *****************Verificador de idade*************************
+  let extrair = idade(anoDigitado, mesDigitado, diaDigitado);
+  if (extrair < 18 || extrair > 130) {
+    alert('você não está apto a realizar o cadastro');
+  } else {
+    alert('seu cadastro foi aprovado');
+  }
+  console.log(idade(anoDigitado, mesDigitado, diaDigitado));
 }
 
 // *****************Banner de Imagens**************************
 
-let vtBanner = ["img/b1.jpg", "img/b2.jpg", "img/b3.jpg", "img/b4.jpg"];
+let vtBanner = ['img/b1.jpg', 'img/b2.jpg', 'img/b3.jpg', 'img/b4.jpg'];
 let max = vtBanner.length - 1;
 let i = 0;
 
@@ -272,40 +253,40 @@ $('#cadastro').click(function () {
     $('nome').focus().css();
   }
 });
-    $("#banner").css("backgroundImage", "url(" + vtBanner[i] + ")").fadeOut(1000,
-        function () {
-            i += opr;
-            if (i > max) {
-                i = 0;
-            } else if (i < 0) {
-                i = max;
-            }
-            $("#banner").css("backgroundImage", "url(" + vtBanner[i] + ")").fadeIn(1000);
-        });
-}
-setInterval(() => troca(1), 3000)
+$('#banner')
+  .css('backgroundImage', 'url(' + vtBanner[i] + ')')
+  .fadeOut(1000, function () {
+    i += opr;
+    if (i > max) {
+      i = 0;
+    } else if (i < 0) {
+      i = max;
+    }
+    $('#banner')
+      .css('backgroundImage', 'url(' + vtBanner[i] + ')')
+      .fadeIn(1000);
+  });
 
+setInterval(() => troca(1), 3000);
 
 // ********************Conta*************************
 $(document).ready(function () {
-    $("#botaoConta").click(function () {
-        // no seletor p, de parágrafo... mostre e esconda o texto com o toggle.
-        $("#apareceLogin").toggle();
-    });
+  $('#botaoConta').click(function () {
+    // no seletor p, de parágrafo... mostre e esconda o texto com o toggle.
+    $('#apareceLogin').toggle();
+  });
 });
 
-$("#fecharLogin").click(function () {
-    $("#apareceLogin").hide(3000);
+$('#fecharLogin').click(function () {
+  $('#apareceLogin').hide(3000);
 });
-
 
 // *****************Validação**************************
 function validacaoCompleta() {
-
-        if (document.formCadastro.nome.value.length < 3) {
-        alert("Preencha campo nome corretamente!");
-        document.formCadastro.nome.focus();
-        return false;
+  if (document.formCadastro.nome.value.length < 3) {
+    alert('Preencha campo nome corretamente!');
+    document.formCadastro.nome.focus();
+    return false;
     // }
     // verificaNasc();
     // if (document.formCadastro.email.value == "" || document.formCadastro.email.value.length < 3) {
@@ -324,9 +305,9 @@ function validacaoCompleta() {
     //     alert("Prencha campo menssagem corrtamente! Use caracteres com bom senso. Excesso de espaços em branco não serão permitidos.");
     //     document.formCadastro.mensagem.focus();
     //     return false;
-    }
+  }
 
-    {
-        alert("Cadastro enviado com sucesso!");
-    }
-};
+  {
+    alert('Cadastro enviado com sucesso!');
+  }
+}
