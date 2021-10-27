4e5f6d21ca52ff7f3561fdd29bc441a313673f56
raunylima <czar.rauny@gmail.com>
2021-10-26 23:41:15 -0300

    ATUALIZAÇÃO - 26-10 23:33
    Falta resolver questões de CSS e Responsivo. Caso sobre tempo fazer questões de troca de cor de linha por Tipo de Operação e Id com Data.

--- js/js_simulador.js
+++ js/js_simulador.js
@@ -24,11 +24,11 @@ class SoulCoin {
             if (this.testeBtn == 0) {
                 this.adicionar(novaOperacao);
             }
-            
+
             else {
                 this.atualizar(this.testeBtn);
             }
-            
+
             this.listarDados();
 
             this.cancelar();
@@ -69,6 +69,10 @@ class SoulCoin {
             imgDelete.setAttribute("onclick", "novaOperacao.deletar(" + this.arrayMoeda[m].id + ")");
 
             imgEdit.setAttribute("onclick", "novaOperacao.mostrarDados(" + JSON.stringify(this.arrayMoeda[m]) + ")");
+
+            $("img:first-child").click(function () {
+                $("#tipoOperacao").attr("disabled", true);
+            })
         }
     }
 
@@ -80,8 +84,9 @@ class SoulCoin {
     cancelar() {
         document.getElementById("tipoOperacao").value = "";
         document.getElementById("quantidadeMoeda").value = "";
-        document.getElementById("btn1").innerText = "Salvar"
+        document.getElementById("btnS").innerText = "Salvar"
         this.testeBtn = 0;
+        $("#tipoOperacao").attr("disabled", false);
     }
 
     lerDados() {
@@ -89,22 +94,34 @@ class SoulCoin {
         let novaOperacao = {};
 
         novaOperacao.operacao = document.getElementById("tipoOperacao").value;
-        console.log(novaOperacao.operacao)
-        
-        var cotacaoHoje = (Math.random()+7).toFixed(4)
 
-        var cotacaoCompra = (Math.sqrt(Math.sqrt(Math.pow((document.getElementById("quantidadeMoeda").value), 3)))+(document.getElementById("quantidadeMoeda").value*cotacaoHoje))
+        // if (novaOperacao.operacao == "Compra") {
+        //     $("#tbody td").css("background", "#ccc");
+        // }
+        // else {
+        //     $("#tbody td").css("background", "#000"); 
+        // }
 
-        var cotacaoVenda = ((cotacaoCompra)-((Math.random()*(cotacaoCompra*0.1))+(cotacaoCompra*0.08)));
+        var cotacaoHoje = (Math.random() + 7).toFixed(4)
+
+        var cotacao2 = Math.random()
+
+        var cotacaoCompra = (Math.sqrt(Math.sqrt(Math.pow((document.getElementById("quantidadeMoeda").value), 3))) + (document.getElementById("quantidadeMoeda").value * cotacaoHoje))
+
+        var cotacaoVenda = ((cotacaoCompra) - ((cotacao2 * (cotacaoCompra * 0.1)) + (cotacaoCompra * 0.08)));
 
         novaOperacao.quantidade = document.getElementById("quantidadeMoeda").value;
         if (novaOperacao.operacao == "Compra") {
-        novaOperacao.valor = cotacaoCompra.toFixed(4)}
+            novaOperacao.valor = cotacaoCompra.toFixed(4)
+        }
         else if (novaOperacao.operacao == "Venda") {
-            novaOperacao.valor = cotacaoVenda.toFixed(4)}
+            novaOperacao.valor = cotacaoVenda.toFixed(4)
+        }
 
         novaOperacao.cotacao = cotacaoHoje
-        
+
+        novaOperacao.cotacao2 = cotacao2
+
         novaOperacao.id = this.id;
 
         return novaOperacao;
@@ -135,6 +152,7 @@ class SoulCoin {
                 }
             }
         }
+        $("#tipoOperacao").attr("disabled", false);
     }
 
     mostrarDados(dados) {
@@ -142,7 +160,7 @@ class SoulCoin {
         document.getElementById("tipoOperacao").value = dados.operacao;
         document.getElementById("quantidadeMoeda").value = dados.quantidade;
 
-        document.getElementById("btn1").innerText = "Atualizar";
+        document.getElementById("btnS").innerText = "Atualizar";
         this.testeBtn = dados.id;
     }
 
@@ -150,11 +168,19 @@ class SoulCoin {
 
         for (let m = 0; m < this.arrayMoeda.length; m++) {
             if (id == this.arrayMoeda[m].id) {
-                this.arrayMoeda[m].operacao = document.getElementById("tipoOperacao").value;
+                this.arrayMoeda[m].operacao = this.arrayMoeda[m].operacao;
                 this.arrayMoeda[m].quantidade = document.getElementById("quantidadeMoeda").value
+                if (this.arrayMoeda[m].operacao == "Compra") {
+                    this.arrayMoeda[m].valor = (Math.sqrt(Math.sqrt(Math.pow(this.arrayMoeda[m].quantidade, 3))) + (this.arrayMoeda[m].quantidade * this.arrayMoeda[m].cotacao)).toFixed(4)
+                }
+                else if (this.arrayMoeda[m].operacao == "Venda") {
+                    this.arrayMoeda[m].valor = (((Math.sqrt(Math.sqrt(Math.pow(this.arrayMoeda[m].quantidade, 3))) + (this.arrayMoeda[m].quantidade * this.arrayMoeda[m].cotacao))) - ((this.arrayMoeda[m].cotacao2 * ((Math.sqrt(Math.sqrt(Math.pow(this.arrayMoeda[m].quantidade, 3))) + (this.arrayMoeda[m].quantidade * this.arrayMoeda[m].cotacao)) * 0.1)) + ((Math.sqrt(Math.sqrt(Math.pow(this.arrayMoeda[m].quantidade, 3))) + (this.arrayMoeda[m].quantidade * this.arrayMoeda[m].cotacao)) * 0.08))).toFixed(4)
+                }
             }
         }
-        document.getElementById("btn1").innerText = "Salvar"
+        $("#tipoOperacao").attr("disabled", false);
+
+        document.getElementById("btnS").innerText = "Salvar"
         this.testeBtn = 0;
     }
 }
